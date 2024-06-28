d4ce2b3a08084979dbaa1814e011b980142b6969
AkaleshTanwer <akaleshtanwer5014@gmail.com>
2024-06-28 16:23:18 +0530

    checked formatter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 const Home = () => {
-  return (
-    <h1 className="text-4xl font-bold px-2 p-2">Next.js App Router</h1>  
-  )
-}
+  return <h1 className="p-2 px-2 text-4xl font-bold">Next.js App Router</h1>;
+};
 
-export default Home;  
+export default Home;
